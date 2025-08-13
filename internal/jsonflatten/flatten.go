ff2004b460c6de3f7d5a9b31d46724063c0569bb
Osamu Habuka <osamu@habuka.jp>
2025-08-13 14:09:48 +0900

    use model.TableRow type for all module signatures for go vet conformity

--- internal/jsonflatten/flatten.go
+++ internal/jsonflatten/flatten.go
@@ -1,7 +1,9 @@
 package jsonflatten
 
+import "github.com/habuka036/csvoid/internal/model"
+
 // FlattenTable flattens a nested JSON structure into a slice of TableRow for CSV/Excel.
-func FlattenTable(root interface{}) []map[string]string {
+func FlattenTable(root interface{}) []model.TableRow {
 	// 実装は後で記述
 	return nil
 }
